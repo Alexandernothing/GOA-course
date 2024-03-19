5ce446f6ce21c0f2ea1e6fb4bc932d9377cd5732
Aleqsandre egutia <egutiaaleqsandre.66@gmail.com>
2024-03-19 23:30:57 +0400

    second commit

---
+++ Day 1/homework 1.py
@@ -0,0 +1,11 @@
+name = "Aleqsandre"
+surname = "Egutia"
+age = 11
+height = 150
+weight = 39
+favorite_sport = "football"
+hobby = "playing videogames"
+favorite_animal = "dog"
+favorite_drink = "water"
+favorite_food = "pizza and khinkali"
+print(name + "  " + (surname) + " " + str(age) + " " + str(height) + " " + str(weight) + " " + (favorite_sport) + " " + (hobby) + " " + (favorite_animal) + " " + )
