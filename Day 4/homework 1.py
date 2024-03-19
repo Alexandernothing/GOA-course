edf05b62a42a4a0c86eac1e89506c2d00af7b4cc
Aleqsandre egutia <egutiaaleqsandre.66@gmail.com>
2024-03-19 23:33:10 +0400

    fifth commit

---
+++ Day 4/homework 1.py
@@ -0,0 +1,14 @@
+phone = input("enter how many phones are in your home: " )
+ 
+tv = input("enter how many tvs are in your home :" )
+
+computer = input("enter how many computers are in your home: " )
+
+ipad = input("enter how many ipads are in your home: " )
+
+laptop = input("enter how many laptops are in your home: " )
+
+
+total = int(phone) + int(tv) + int(computer) + int(ipad) + int(laptop)
+
+print("the total is: ", total)
---
+++ Day 4/homework 2.py
@@ -0,0 +1,10 @@
+name = input("please enter your name: ")
+
+age = input("pleaes enter your age: ")
+
+time_travel = input("please enter how many years into the future do you want to go: ")
+
+total = int(age) + int(time_travel)
+
+print(name,", in that time you will be", total )
+      
