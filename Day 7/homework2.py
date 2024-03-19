22d70004af752d172237b57ac8bcef056822e5a8
Aleqsandre egutia <egutiaaleqsandre.66@gmail.com>
2024-03-19 23:38:30 +0400

    eighth commit

---
+++ Day 7/homework2.py
@@ -0,0 +1,3 @@
+age = int(input("Please enter your age: "))
+
+print(age < 20 and age > 12)
--- Day 7/homework5.py
+++ Day 7/homework5.py
@@ -1,11 +1,17 @@
 #And
 print(7 > 8 and 78 != 78)   #FALSE and FALSE = FALSE
 
-apple = "green"
-apple = False
-banana = "yellow"
-banana = True
-print(apple and banana)
+print(7 == 0 and 56 < 654)  #FALSE and TRUE = FALSE
 
+print(5 > 3 and 45 > 67)    #TRUE and FALSE = FALSE
 
+print(54 >23 and 87 < 120)  #TRUE and TRUE = TRUE
 
+#Or
+print(76 != 76.00 or 34 > 100)   #FALSE or FALSE = FALSE
+
+print(42 < 32 or 67 >= 66.9999)  #FALSE or TRUE = TRUE
+
+print(10 <= 10 or 34 != 34)     #TRUE or FALSE = TRUE
+
+print(20 <= 24 or 86 >= 67)   #TRUE or TRUE = TRUE
