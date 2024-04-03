90c608817743992fd54a40b6141a360a88647622
Aleqsandre egutia <egutiaaleqsandre.66@gmail.com>
2024-04-03 12:07:22 +0400

    tenth commit

---
+++ Day 9/homework1.py
@@ -0,0 +1,8 @@
+wage = int(input("Enter your wage please: "))
+
+if wage > 10000:
+    print("Did you study in GOA?")
+elif wage < 10000 and wage > 1000:
+    print("you MID")
+else:
+    print("You should have entered GOA, matriciel ")
