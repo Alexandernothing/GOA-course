65f53f045370a8c4aebcd9564526edc623e71947
Aleqsandre egutia <egutiaaleqsandre.66@gmail.com>
2024-06-07 01:23:39 +0400

    19th commit

---
+++ GOA classworks/Day 18/classwork1.py
@@ -0,0 +1,2 @@
+sports = ["football", "rugby", "basketball", "hockey", "tennis"]    #აქ დავწერეთ სპორტის ჩამონათვალი
+print(sports{-1, -2, -3})
---
+++ GOA classworks/Day 18/classwork3.py
@@ -0,0 +1,2 @@
+movies = ["The Shawshank Redemption", "The Godfather", "The Dark Knight", "The Godfather Part II", "Angry Men", "Schindler's List", "The Lord of the Rings: The Return of the King", "Pulp Fiction", "The Lord of the Rings: The Fellowship of the Ring", "The Good, the Bad and the Ugly"]
+print(movies[2:11:3])
