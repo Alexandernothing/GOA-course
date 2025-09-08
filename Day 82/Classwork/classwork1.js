4bf024543ed577df4d1157d12cfe1d0469117218
Alexandernothing <egutiaaleqsandre.66@gmail.com>
2025-09-08 23:50:43 +0400

    commit

---
+++ Day 82/Classwork/classwork1.js
@@ -0,0 +1,8 @@
+// 1. შექმენით ცვლადები var/let/const-ით და კონსოლში გამოიტანეთ.
+
+var name = 'Aleqsandre'
+let surname = 'Egutia'
+const age = 13
+console.log(name)
+console.log(surname)
+console.log(age)
---
+++ Day 82/Homework/homework5.js
@@ -0,0 +1,13 @@
+// 5. შექმენით 2 ცვლადი სადაც შეინახავთ Number ტიპის მონაცემებს. კონსოლში გამოიტანეთ ამ ორ რიცხვზე შესრულებული ყველა მათემატიკური მოქმედება.
+
+let num1 = 24
+let num2 = 684.767
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num2 - num1)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num2 / num1)
+console.log(num1 % num2)
+console.log(num2 % num1)
